a53309c8e430bbe657fd6b08af37b359990e9483
wisthy <wisthy@users.noreply.github.com>
2014-08-11 11:10:40 +0200

    Update ABaseEntity.java
    adapt hiberniate inheritance behavior

--- src/main/java/be/shoktan/BeeBreedingManager/model/ABaseEntity.java
+++ src/main/java/be/shoktan/BeeBreedingManager/model/ABaseEntity.java
@@ -11,7 +11,7 @@ import org.hibernate.annotations.GenericGenerator;
  * @author Wisthy
  *
  */
-@Entity
+@Entity @Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 public abstract class ABaseEntity {
 	@Id
 	@GeneratedValue(generator="increment")
