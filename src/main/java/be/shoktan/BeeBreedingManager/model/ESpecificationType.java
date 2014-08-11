8e4f92841d80aa9bccc7961eebcf534fac2b898a
wisthy <wisthy@users.noreply.github.com>
2014-08-11 16:03:40 +0200

    Create ESpecificationType.java
    Enumeration to represent all the sub-types of the partition below Specification

---
+++ src/main/java/be/shoktan/BeeBreedingManager/model/ESpecificationType.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package be.shoktan.BeeBreedingManager.model;
+
+/**
+ * Enumeration to represent all the sub-types of the partition below Specification
+ * @author Wisthy
+ *
+ */
+public enum ESpecificationType {
+	specie, group
+}
