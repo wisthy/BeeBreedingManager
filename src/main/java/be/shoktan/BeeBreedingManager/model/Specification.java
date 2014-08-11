e48a0a7f0fbbbfb78972a691e4e8eb53b9dacb46
wisthy <wisthy@users.noreply.github.com>
2014-08-11 11:09:55 +0200

    Update Specification.java
    adding javadoc

--- src/main/java/be/shoktan/BeeBreedingManager/model/Specification.java
+++ src/main/java/be/shoktan/BeeBreedingManager/model/Specification.java
@@ -2,6 +2,12 @@ package be.shoktan.BeeBreedingManager.model;
 
 import javax.persistence.Entity;
 
+/**
+ * Class used to represent of bee or a group of bees
+ * usefull for mutation representation
+ * @author Wisthy
+ *
+ */
 @Entity @Inheritance(strategy=InheritanceType.JOINED)
 public class Specification {
 	
