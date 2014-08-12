cd4a35479ec86d705fa7f2631015077edd127e65
wisthy <wisthy@users.noreply.github.com>
2014-08-12 10:51:03 +0200

    Update Specie.java
    implement iterable

--- src/main/java/be/shoktan/BeeBreedingManager/model/Specie.java
+++ src/main/java/be/shoktan/BeeBreedingManager/model/Specie.java
@@ -1,5 +1,7 @@
 package be.shoktan.BeeBreedingManager.model;
 
+import java.util.Iterator;
+
 import javax.persistence.Entity;
 
 /**
@@ -17,6 +19,59 @@ public class Specie extends Specification {
 	@Override
 	protected ESpecificationType getType() {
 		return ESpecificationType.specie;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see be.shoktan.BeeBreedingManager.model.Specification#iterator()
+	 */
+	@Override
+	public Iterator<Specification> iterator() {
+		return new SpecieIterator(this);
+	}
+	
+	private class SpecieIterator implements Iterator<Specification>{
+		private boolean done = false;
+		private Specie root;
+		
+		/**
+		 * @param root
+		 */
+		public SpecieIterator(Specie root) {
+			super();
+			this.root = root;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see java.util.Iterator#hasNext()
+		 */
+		@Override
+		public boolean hasNext() {
+			return done;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see java.util.Iterator#next()
+		 */
+		@Override
+		public Specification next() {
+			done = true;
+			return root;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see java.util.Iterator#remove()
+		 */
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		
 	}
 
 }
