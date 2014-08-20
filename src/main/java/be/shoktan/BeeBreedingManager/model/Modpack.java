7e64a72b15a909d3c15c8bb6ce890bd4754d5e72
wisthy <wisthy@users.noreply.github.com>
2014-08-20 10:39:49 +0200

    Update Modpack.java
    add BeeCollection link and improve validation function

--- src/main/java/be/shoktan/BeeBreedingManager/model/Modpack.java
+++ src/main/java/be/shoktan/BeeBreedingManager/model/Modpack.java
@@ -1,6 +1,11 @@
 package be.shoktan.BeeBreedingManager.model;
 
 import java.util.Collection;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +17,12 @@ import org.slf4j.LoggerFactory;
  */
 public class Modpack extends ANamedEntity {
 	private static final Logger LOG = LoggerFactory.getLogger(Modpack.class);
+	
+	@ManyToMany(mappedBy = "modpack")
 	private Collection<PluginVersion> plugins;
+	
+	@OneToMany(mappedBy = "modpack")
+	private Collection<BeeCollection> collections;
 	
 	
 	
@@ -32,25 +42,51 @@ public class Modpack extends ANamedEntity {
 		this.plugins = plugins;
 	}
 	
+	/**
+	 * @return the collections
+	 */
+	public Collection<BeeCollection> getCollections() {
+		return collections;
+	}
+
+	/**
+	 * @param collections the collections to set
+	 */
+	public void setCollections(Collection<BeeCollection> collections) {
+		this.collections = collections;
+	}
+	
 	
 	
 	// ********** others ********** //
 	
+
+
 	/**
 	 * run validations check on the plugins list to ensure it is correct.
 	 */
 	public boolean validatePluginList(){
 		if(LOG.isTraceEnabled())LOG.trace("starting validation of the plugin list");
 		boolean isValid = true;
+		
+		SortedSet<EPlugin> pluginSet = new TreeSet<>(); 
+		
 		for(PluginVersion pluginVersion : plugins){
 			if(pluginVersion == null){
 				LOG.error("there is a null plugin in the list");
 				isValid = false;
 				continue;
 			}
-			pluginVersion.validate();
+			EPlugin plugin = pluginVersion.getPlugin();
+			if(pluginSet.contains(plugin)){
+				LOG.error("the plugin <"+plugin+"> is present more than one time");
+				isValid = false;
+			}else{
+				pluginSet.add(plugin);
+			}
+			isValid = pluginVersion.validate() && isValid;
 		}
-		if(LOG.isTraceEnabled())LOG.trace("end of the validation of the plugin list");
+		if(LOG.isTraceEnabled())LOG.trace("end of the validation of the plugin list with result "+isValid);
 		return isValid;
 	}
 }
