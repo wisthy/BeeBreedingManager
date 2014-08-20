9de3e2eac1cc28ef93397a17f14bfff6c2990547
wisthy <wisthy@users.noreply.github.com>
2014-08-20 10:40:48 +0200

    Update BeeCollection.java
    add specie link and "add" method

--- src/main/java/be/shoktan/BeeBreedingManager/model/BeeCollection.java
+++ src/main/java/be/shoktan/BeeBreedingManager/model/BeeCollection.java
@@ -1,8 +1,10 @@
-/**
- * 
- */
 package be.shoktan.BeeBreedingManager.model;
 
+import java.util.Collection;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 /**
@@ -13,6 +15,12 @@ import javax.persistence.ManyToOne;
 public class BeeCollection extends ANamedEntity{
 	@ManyToOne
 	private User owner;
+	
+	@ManyToOne
+	private Modpack modpack;
+	
+	@ManyToMany
+	private SortedSet<Specie> ownedSpecies;
 	
 	
 	
@@ -31,8 +39,54 @@ public class BeeCollection extends ANamedEntity{
 	public void setOwner(User owner) {
 		this.owner = owner;
 	}
-	
+
+	/**
+	 * @return the modpack
+	 */
+	public Modpack getModpack() {
+		return modpack;
+	}
+
+	/**
+	 * @param modpack the modpack to set
+	 */
+	public void setModpack(Modpack modpack) {
+		this.modpack = modpack;
+	}
+
+	/**
+	 * @return the ownedSpecies
+	 */
+	public Collection<Specie> getOwnedSpecies() {
+		return ownedSpecies;
+	}
+
+	/**
+	 * @param ownedSpecies the ownedSpecies to set
+	 */
+	public void setOwnedSpecies(SortedSet<Specie> ownedSpecies) {
+		this.ownedSpecies = ownedSpecies;
+	}
 	
 	
 	// ********** others ********** //
+	
+	/**
+	 * do the initialization of the ownedSpecies set if necessary
+	 */
+	private void initOwnedSpecies(){
+		if(this.ownedSpecies == null){
+			this.ownedSpecies = new TreeSet<>();
+		}
+	}
+	
+	/**
+	 * add the specie to the set of owned species
+	 * @param specie the specie to add
+	 * @return true if the specie has been added, false elsewhere
+	 */
+	public boolean addSpecie(Specie specie){
+		initOwnedSpecies();
+		return this.ownedSpecies.add(specie);
+	}
 }
